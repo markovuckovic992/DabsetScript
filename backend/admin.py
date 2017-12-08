0bbc9a0f8b46ca25567e733f21963ebb8c5cc911
markovuckovic992 <markovuckovic992@yahoo.com>
2017-12-08 15:17:38 +0100

    changes

--- backend/admin.py
+++ backend/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from backend.models import ActionLogs
 
 # Register your models here.
+class ActionLogsAdmin(admin.ModelAdmin):
+    list_display = ['lead__domain', 'field_thats_changed', 'old_value', 'new_value']
+    search_fields = ['lead__domain', 'field_thats_changed', 'old_value', 'new_value']
+
+admin.site.register(ActionLogs, ActionLogsAdmin)
