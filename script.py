23e55f7b62f2741434cc6d9cb05285ab97bcf810
marko <Marko Vuckovic>
2017-12-12 23:34:51 +0100

    fixes

--- script.py
+++ script.py
@@ -1,73 +1,81 @@
 import requests
 from bs4 import BeautifulSoup as BS
 from os import popen
-from backend.models import Lead, LeadType
+from selenium import webdriver
+import json
+# from backend.models import Lead, LeadType
 
 
-def saveData(dicts):
-    for dict_ in dicts:
-        new_lead = Lead(**dict_)
-        new_lead.save()
+# def saveData(dicts):
+#     for dict_ in dicts:
+#         new_lead = Lead(**dict_)
+#         new_lead.save()
 
 
 def checkIsMobileFriendly(domain):
-    url = 'http://' + domain
+    url = 'https://www.googleapis.com/pagespeedonline/v3beta1/mobileReady?url=http://' + domain
     resp = requests.get(url)
     resp = resp.json()
-    return resp['is_mobile_friendly']
+
+    # file = open('json.json', 'w')
+    # file.write(json.dumps(resp))
+    # file.close()
+
+    return resp['ruleGroups']['USABILITY']['pass']
+    # return resp['is_mobile_friendly']
 
 
 def checkIfHasVideo(domain):
-    url = 'http://' + domain
-    resp = requests.get(url)
-    soup = BS(resp.content)
-    videos = soup.findAll("embed","object","param","video")
-    print videos
+    url = 'https://' + domain
+    driver = webdriver.PhantomJS()
+    driver.get(url)
+    soup = BS(driver.page_source, 'lxml')
+    videos = soup.find_all(["embed","object","param","video", "iframe"])
     if len(videos):
         return True
     else:
         return False
 
-def getEmail(domain):
-    tube = popen('./whois.sh ' + domain)
-    response = tube.read()
-    return response.replace('Registrant Email:', '').lstrip().rstrip()
+# def getEmail(domain):
+#     tube = popen('./whois.sh ' + domain)
+#     response = tube.read()
+#     return response.replace('Registrant Email:', '').lstrip().rstrip()
 
 
-def process(keywords, zone_files):
-    lead_type = LeadType.objects.get(name="raw_lead")
-    dicts = []
+# def process(keywords, zone_files):
+#     lead_type = LeadType.objects.get(name="raw_lead")
+#     dicts = []
 
-    for file in zone_files:
-        keywords = sorted(keywords, key=len, reverse=True)
-        tube = popen('./getLines.sh ' + file + ' ' + keywords[0])
-        matched_lines = set(tube.read().split())
-        tube.close()
+#     for file in zone_files:
+#         keywords = sorted(keywords, key=len, reverse=True)
+#         tube = popen('./getLines.sh ' + file + ' ' + keywords[0])
+#         matched_lines = set(tube.read().split())
+#         tube.close()
 
-        if len(keywords) > 1:
-            for keyword in keywords[1:]:
-                matched_lines = [line.lower() for line in matched_lines if keyword.lower() in line.lower()]
+#         if len(keywords) > 1:
+#             for keyword in keywords[1:]:
+#                 matched_lines = [line.lower() for line in matched_lines if keyword.lower() in line.lower()]
 
-        for line in matched_lines:
-            is_mobile_friendly = False
-            has_video = False
-            if checkIsMobileFriendly(line):
-                is_mobile_friendly = True
-            if checkIfHasVideo(line):
-                has_video = True
+#         for line in matched_lines:
+#             is_mobile_friendly = False
+#             has_video = False
+#             if checkIsMobileFriendly(line):
+#                 is_mobile_friendly = True
+#             if checkIfHasVideo(line):
+#                 has_video = True
 
-            mail = getEmail(line)
-            dicts.append({
-                'keywords': keywords,
-                'domain': line,
-                'mail': mail,
-                'has_video': has_video,
-                'is_mobile_friendly': is_mobile_friendly,
-                'lead_type': lead_type,
-                'datetime_of_last_change': timezone.now(),
-            })
+#             mail = getEmail(line)
+#             dicts.append({
+#                 'keywords': keywords,
+#                 'domain': line,
+#                 'mail': mail,
+#                 'has_video': has_video,
+#                 'is_mobile_friendly': is_mobile_friendly,
+#                 'lead_type': lead_type,
+#                 'datetime_of_last_change': timezone.now(),
+#             })
 
-    saveData(dicts)
+#     saveData(dicts)
 
 
 def test(url):
@@ -76,28 +84,31 @@ def test(url):
     from selenium.webdriver.support import expected_conditions as EC
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
-
-    from selenium import webdriver
+    
     import os
     import time
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
 
-    driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    driver = webdriver.PhantomJS()
     driver.get(url)
-    # time.sleep(25)
-    # divs = driver.find_elements_by_class_name("html5-video-container")
+    soup = BS(driver.page_source, 'lxml')
+    divs = soup.findAll("iframe")
+    print divs
 
-    try:
-        element = WebDriverWait(driver, 1).until(
-            EC.presence_of_element_located((By.ID, "page-container"))
-        )
-        time.sleep(100)
+    # try:
+    #     element = WebDriverWait(driver, 1).until(
+    #         EC.presence_of_element_located((By.ID, "page-container"))
+    #     )
+    #     time.sleep(100)
 
-        divs = driver.find_elements_by_class_name("html5-video-container")
-        print divs
-    finally:
-        pass
+    #     divs = driver.findAll("iframe")
+    #     print divs
+    # finally:
+    #     pass
 
-    print '--------------'
+    # print '--------------'
+
+if __name__ == "__main__":
+    print checkIsMobileFriendly('fplalerts.com/')
